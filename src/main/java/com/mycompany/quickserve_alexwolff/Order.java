54f9402d3ba2eded9f81afa35dc6230a53a33646
st10508450 <Alexmainwolff@gmail.com>
2026-03-06 21:07:41 +0200

    Added calculations and constant for vat and discounts
    Made methods for VAT and Discounts to be used in the step for the receipt

--- src/main/java/com/mycompany/quickserve_alexwolff/Order.java
+++ src/main/java/com/mycompany/quickserve_alexwolff/Order.java
@@ -10,24 +10,47 @@ package com.mycompany.quickserve_alexwolff;
  * @author Alex Wolff
  */
 public class Order {
-    String custormerName;
+    String customerName;
     String studentNumber;
     String itemOrder;
     int quantity;
     double pricePerItem;
-    String customerName;
-    
-    
-    // Attributes for my order method
-    Order(){
-    this.custormerName = custormerName;
-    this.studentNumber = studentNumber;
-    this.itemOrder = itemOrder;
-    this.quantity = quantity;
-    this.pricePerItem = pricePerItem;    
-    
-    
-    
-    
-    }   
+    boolean discountApplied = false;
+
+    //constant for VAT
+    static final double VAT_RATE = 0.15;
+
+    //empty constructor
+    public Order() {}
+
+    //method for calculation of subtotal
+    public double calculateSubtotal() {
+        return quantity * pricePerItem;
+    }
+
+    //method for VAT calculation
+    public double calculateVAT(double subtotalAfterDiscount) {
+        return subtotalAfterDiscount * VAT_RATE;
+    }
+
+    //method for discount calculation
+    public double applyDiscount() {
+        if (quantity > 3) {
+            discountApplied = true;
+            return calculateSubtotal() * 0.10; // 10% discount
+        } else {
+            discountApplied = false;
+            return 0.0;
+        }
+    }
+
+    //method for final total
+    public double calculateTotal() {
+        double subtotal = calculateSubtotal();
+        double discount = applyDiscount();
+        double subtotalAfterDiscount = subtotal - discount;
+        double vat = calculateVAT(subtotalAfterDiscount);
+        return subtotalAfterDiscount + vat;
+    }
 }
+
