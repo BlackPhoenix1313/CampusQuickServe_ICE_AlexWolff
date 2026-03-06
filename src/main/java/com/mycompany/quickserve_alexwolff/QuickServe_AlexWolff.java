8415b3900ad29af083230c5b95297b25fa8c6a16
st10508450 <Alexmainwolff@gmail.com>
2026-03-06 17:08:54 +0200

    Testing Commit
    Hope this is right

---
+++ src/main/java/com/mycompany/quickserve_alexwolff/QuickServe_AlexWolff.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickserve_alexwolff;
+
+/**
+ *
+ * @author Alex Wolff
+ */
+public class QuickServe_AlexWolff {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //testing push and confirm
+    }
+}
